4ef58a44e268c5b23c295603f087b07cd0423d80
Sabir Mustafaev <114620144+SabirAga@users.noreply.github.com>
2024-06-04 01:57:10 +0200

    test of fixes

--- components/main-header.js
+++ components/main-header.js
@@ -55,7 +55,7 @@ export const MainHeader = () => {
             shrunk ? "md:-translate-x-7" : ""
           }`}
         >
-          <div className="lg:ml-[14px] lg:p-[10px] lg:pt-[20px] lg:mr-[2px] lg:static absolute md:right-10 right-5 top-10">
+          <div className="lg:ml-[14px] lg:p-[10px] lg:pt-[20px] lg:mr-[2px] lg:static absolute md:right-10 right-5 top-5">
             <DrawerComp />
           </div>
           <nav>
