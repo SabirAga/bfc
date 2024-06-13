782e98c5b84cfb2d19b835280e284f008b40f9ed
Sabir Mustafaev <sabiraga@Sabirs-MacBook-Pro.local>
2024-06-13 19:48:21 +0200

    test

--- components/ui/carouselComponent.js
+++ components/ui/carouselComponent.js
@@ -1,6 +1,4 @@
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+
 import Image from "next/image";
 import React from "react";
 import { useTranslation } from "react-i18next";
